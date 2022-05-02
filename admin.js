a6079171e0c217e6c2f20f7d2dec273ff57d6d70
codingscode <heronnestg@hotmail.com>
2022-05-02 16:15:09 -0300

    parte 2

--- admin.js
+++ admin.js
@@ -1,3 +1,4 @@
+/* 
 
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('@admin-bro/express')
@@ -18,6 +19,51 @@ server
   .listen(5500, () => console.log('Servidor iniciado'))
 
 
+
+*/
+
+const mongoose = require('mongoose')
+
+const ProjectSchema = new mongoose.Schema({
+   title: {
+      type: String, required: true
+   },
+   description: String,
+   completed: Boolean,
+   created_at: { type: Date, default: Date.now }
+})
+
+const Project = mongoose.model('Project', ProjectSchema)
+
+
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
+AdminBro.registerAdapter(AdminBroMongoose)
+
+
+const adminBroOptions = new AdminBro({
+   resources: [Project],
+   rootPath: '/admin'
+})
+
+const router = AdminBroExpress.buildRouter(adminBroOptions)
+
+const express = require('express')
+const server = express()
+
+server.use(adminBroOptions.options.rootPath, router)
+
+const run = async () => {
+   await mongoose.connect('mongodb://localhost/adminbroapp', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
+   await server.listen(5500, () => console.log('servidor iniciado'))
+}
+
+run()
 
 
 
